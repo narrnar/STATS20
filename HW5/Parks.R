ea71fb344482d027ef25ea37a4a064c17b411271
narrnar <100457071+narrnar@users.noreply.github.com>
2024-02-08 16:05:19 -0800

    HW5

---
+++ HW5/Parks.R
@@ -0,0 +1,4 @@
+parks_df <-
+structure(list(Name = c("Leslie", "Ron", "April"), Height = c(62, 
+71, 66), Weight = c(115, 201, 119), Income = c(4000, NA, 4000
+)), class = "data.frame", row.names = c(NA, -3L))
